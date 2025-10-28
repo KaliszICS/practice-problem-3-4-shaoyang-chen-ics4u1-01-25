2e2f01d1441c04238f0cad0bba296d83786a1cd5
Shaoyang Chen <shaoyang.chen@student.tdsb.on.ca>
2025-10-28 08:05:24 -0400

    Update PracticeProblem.java

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -7,34 +7,37 @@ public class PracticeProblem {
 
 	}
 
-	public static Set<Integer> seen;
-    public static int[] result;
-    public static int[] recaman(int n){
-        if (n == 0) return new int[0];
-        if (n == 1) return new int[]{1};
+	public static int[] recaman(int n) {
+        //sp
+        if (n <= 0) {
+            return new int[0];
+        }
+        //seen number set and result set
         int[] result = new int[n];
-        result[0] = 0;
         Set<Integer> seen = new HashSet<>();
-        generateRecaman(1, 0);
+        generateRecaman(1, 0, result, seen);
+
         return result;
     }
-    public static void generateRecaman(int start, int prev) {
-        if (start > result.length) {
+
+    public static void generateRecaman(int current, int prev, int[] result, Set<Integer> seen) {
+        // sp: completed
+        if (current > result.length) {
             return;
         }
-        int testValue = prev - start;
-
-        // if testValue meet requirements
+        // test value
+        int testValue = prev - current;
+        // meet requirement case1
         if (testValue > 0 && !seen.contains(testValue)) {
-            result[start - 1] = testValue;
+            result[current - 1] = testValue;
             seen.add(testValue);
         } else {
-            // else case
-            testValue = prev + start;
-            result[start - 1] = testValue;
+            // case2
+            testValue = prev + current;
+            result[current - 1] = testValue;
             seen.add(testValue);
         }
-        generateRecaman(start +1, testValue);
+        generateRecaman(current + 1, testValue, result, seen);
     }
 
 }
